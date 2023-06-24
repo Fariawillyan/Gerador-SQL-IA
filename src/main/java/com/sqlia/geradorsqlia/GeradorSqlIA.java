8dbef60e7538b7f55ba777351709a5fa4c1c819e
fariawillyan <fariawillyan@gmail.com>
2023-06-24 15:11:21 -0300

    testes

--- src/main/java/com/sqlia/geradorsqlia/GeradorSqlIA.java
+++ src/main/java/com/sqlia/geradorsqlia/GeradorSqlIA.java
@@ -1,5 +1,6 @@
 package com.sqlia.geradorsqlia;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
@@ -8,66 +9,58 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/")
 public class GeradorSqlIA {
 
+        private static final Logger logger = LoggerFactory.getLogger(GeradorSqlIA.class);
         private static final String OPENAI_API_URL = "https://api.openai.com/v1/engines/davinci/completions";
-        private static final String OPENAI_API_TOKEN = "XXXXXXX";
+        private static final String OPENAI_API_TOKEN = "Passar CHAVE aqui"; //TESTAR PROJETO VIA POSTMAN. JSON Exemplo no resourses
 
-        @GetMapping("/generate-sql")
-        public ResponseEntity<String> generateSqlQuery() {
-                CloseableHttpClient httpClient = HttpClientBuilder.create().build();
-                HttpPost request = new HttpPost(OPENAI_API_URL);
+        @PostMapping("/generate-sql")
+        public ResponseEntity<String> generateSqlQuery(@RequestBody String requestBody) {
+                try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
+                        HttpPost request = new HttpPost(OPENAI_API_URL);
 
-                // Defina o token de autenticação no cabeçalho da solicitação
-                request.addHeader("Authorization", "Bearer " + OPENAI_API_TOKEN);
-                request.addHeader("model", "text-curie-001");
+                        // Define o token de autenticação no cabeçalho da solicitação
+                        request.addHeader("Authorization", "Bearer " + OPENAI_API_TOKEN);
+                        request.addHeader("model", "text-curie-001");
 
-                // Defina o corpo da solicitação
-                String requestBody = """
-                {
-                  "prompt": "### Postgres SQL tables, with their properties:\\n#\\n# Employee(id, name, department_id)\\n# Department(id, name, address)\\n# Salary_Payments(id, employee_id, amount, date)\\n#\\n### A query to list the names of the departments which employed more than 10 employees in the last 3 months\\nSELECT",
-                  "temperature": 0,
-                  "max_tokens": 150,
-                  "top_p": 1.0,
-                  "frequency_penalty": 0.0,
-                  "presence_penalty": 0.0,
-                  "stop": ["#", ";"]
-                }
-                """;
-                StringEntity entity = new StringEntity(requestBody, ContentType.APPLICATION_JSON);
-                request.setEntity(entity);
+                        // Define o corpo da solicitação
+                        StringEntity entity = new StringEntity(requestBody, ContentType.APPLICATION_JSON);
+                        request.setEntity(entity);
 
-
-                try {
                         HttpResponse response = httpClient.execute(request);
                         int statusCode = response.getStatusLine().getStatusCode();
-                        System.out.println("Response Code: " + statusCode);
+                        logger.info("Response Code: {}", statusCode);
 
                         HttpEntity responseEntity = response.getEntity();
                         if (responseEntity != null) {
                                 String responseBody = EntityUtils.toString(responseEntity);
-                                System.out.println("Response Body: " + responseBody);
+                                logger.info("Response Body: {}", responseBody);
 
                                 // Converter a resposta em JSON
                                 ObjectMapper objectMapper = new ObjectMapper();
-                                Object json = objectMapper.readValue(responseBody, Object.class);
-                                String jsonResponse = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(json);
+                                JsonNode responseJson = objectMapper.readTree(responseBody);
 
-                                return ResponseEntity.ok(jsonResponse);
+                                // Obter o texto da resposta
+                                String text = responseJson.get("choices").get(0).get("text").asText();
+
+                                return ResponseEntity.ok(text);
                         }
-                } catch (Exception e) {
-                        e.printStackTrace();
+                } catch (IOException e) {
+                        logger.error("Falha ao gerar SQL", e);
                 }
 
-                return ResponseEntity.status(500).body("Failed to generate SQL query");
+                return ResponseEntity.status(500).body("Falha ao gerar SQL");
         }
 }
