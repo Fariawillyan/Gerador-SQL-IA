1304e488987eb805c7b0dd8ce038d8a824e46c10
fariawillyan <fariawillyan@gmail.com>
2023-06-24 13:13:42 -0300

    alteracao

--- src/main/java/com/sqlia/geradorsqlia/GeradorSqlIA.java
+++ src/main/java/com/sqlia/geradorsqlia/GeradorSqlIA.java
@@ -8,25 +8,34 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.net.http.HttpClient;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+@RestController
+@RequestMapping("/api/")
 public class GeradorSqlIA {
 
-        public static void main(String[] args) {
+        private static final String OPENAI_API_URL = "https://api.openai.com/v1/engines/davinci/completions";
+        private static final String OPENAI_API_TOKEN = "xxx";
+
+        @GetMapping("/generate-sql")
+        public ResponseEntity<String> generateSqlQuery() {
                 CloseableHttpClient httpClient = HttpClientBuilder.create().build();
-                HttpPost request = new HttpPost("https://api.openai.com/v1/engines/davinci/completions");
+                HttpPost request = new HttpPost(OPENAI_API_URL);
 
                 // Defina o token de autenticação no cabeçalho da solicitação
-                request.addHeader("Authorization", "Bearer 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");
-                //criar chave secreta no perfil login openIA
+                request.addHeader("Authorization", "Bearer " + OPENAI_API_TOKEN);
 
                 // Defina o parâmetro do modelo
                 request.addHeader("model", "text-davinci-003");
 
                 // Defina o corpo da solicitação
                 String requestBody = "{\n" +
-                        " \"prompt\": \"select from where \",\n" +
+                        " \"prompt\": \"era uma vez \",\n" +
                         "  \"max_tokens\": 50\n" +
                         "}";
                 StringEntity entity = new StringEntity(requestBody, ContentType.APPLICATION_JSON);
@@ -41,9 +50,18 @@ public class GeradorSqlIA {
                         if (responseEntity != null) {
                                 String responseBody = EntityUtils.toString(responseEntity);
                                 System.out.println("Response Body: " + responseBody);
+
+                                // Converter a resposta em JSON
+                                ObjectMapper objectMapper = new ObjectMapper();
+                                Object json = objectMapper.readValue(responseBody, Object.class);
+                                String jsonResponse = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(json);
+
+                                return ResponseEntity.ok(jsonResponse);
                         }
                 } catch (Exception e) {
                         e.printStackTrace();
                 }
+
+                return ResponseEntity.status(500).body("Failed to generate SQL query");
         }
 }
